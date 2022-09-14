ac6560d972ae15b80cf46dedf8e47f276b066223
Marcelo Pinto <99257490+pinto-marcelo@users.noreply.github.com>
2022-09-14 14:19:56 -0300

    Initial commit

---
+++ src/components/TaskList/TaskItem/TaskItem.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./task-item.css";
+
+export default function TaskItem({
+  id,
+  title,
+  taskState,
+  onTaskUpdate,
+  onDeleteTask
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editableTitle, setEditableTitle] = useState(title);
+
+  const ontitleChange = (event) => {
+    const newTitle = event.target.value;
+    setEditableTitle(newTitle);
+    onTaskUpdate(id, newTitle, taskState);
+  };
+
+  const onKeyPress = (event) => {
+    if (event.charCode === 13) {
+      setIsEditing(false);
+      if (editableTitle.lenght === 0) {
+        onDeleteTask(id);
+      }
+    }
+  };
+
+  const onTaskStateChange = (event) => {
+    onTaskUpdate(id, title, event.target.value);
+  };
+
+  if (isEditing) {
+    return (
+      <div className="task-item">
+        <input
+          type="text"
+          value={editableTitle}
+          onChange={ontitleChange}
+          onKeyPress={onKeyPress}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="task-item">
+        <div onClick={(e) => setIsEditing(true)}> {editableTitle} </div>
+        <select onChange={onTaskStateChange} value={taskState}>
+          <option value="Pendente">Pendente</option>
+          <option value="Fazendo">Fazendo</option>
+          <option value="Concluído">Concluído</option>
+        </select>
+      </div>
+    );
+  }
+}
+
+TaskItem.PropTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  taskState: PropTypes.string.isRequired
+};
